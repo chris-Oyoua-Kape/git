57c95843dca60ac3a0fa3946fab5d293e902cf92
chrislaplateforme <chris.oyoua-kape@laplateforme.io>
2024-11-05 10:01:01 +0100

    projet python

---
+++ Python/main.py
@@ -0,0 +1,108 @@
+#for i in range(ord('a'), ord('z') +1) :
+ #   print(f"{chr(i)}," , end = "")
+##################################################
+
+#for i in range(ord('Z'), ord('A') -1, -1):
+ #   print(f"{chr(i)}," , end = "")
+
+################################################
+
+#ma_string = "Je suis un String"
+#print(ma_string)
+
+#################################################
+
+#num1 = 40
+#num2 = 2
+#print(num1 + num2)
+
+#################################################
+
+#num1 = 3
+#num2 = 14
+#print(num1 * num2)
+
+################################################
+
+#produit = {
+
+    #'nom_produit' : 'T_shirt',
+    #'prix_unitaire' : 15.99,
+    #'quantite_stock' : True,
+#}
+
+#print(f"nom_produit : {produit['nom_produit']}")
+#print(f"prix_unitaire : {produit['prix_unitaire']: .2f} euros")
+#print(f"quantite_stock: {'Oui' if produit['quantite_stock'] else 'Non'}")
+
+#x= "hello"
+#y= "word"
+#print(x,y)
+
+###########################################################################
+#for i in range(0,20):
+ #   print(i)
+
+#for i in range(21):
+ #   if i % 2 == 0:
+  #      print(i)
+
+#for i in range(1, 20):
+    #print(f"Le carre est {i ** 2}")
+#for i in range(1, 21):
+ #   print(f"Le carré de {i} est {i**2}")
+
+
+#a = int(input("Entrer une valeur superieur a:"))
+#print("Voici sa table de multiplication de ", a)
+#for i in range(1, 11):
+ #   print(i," * ", a, " = ", i  * a) 
+
+##################################################
+
+#valeur1 = int(input("entrer une premier valeur"))
+#valeur2 = int(input("entrer une deuxième valeurs"))
+#if (valeur1 == valeur2):
+ #   print("valeur1 est égale à valeur2")   
+#else : 
+ #   print("les deux valeur ne sont pas égales")
+###################################################
+
+#a = int(input("entrer votre âge !: "))
+#if a >= 18 : 
+ #   print("Tu peux vote !")
+#else :
+ #   print("tu ne peux pas voté")
+###################################################
+
+#for i in range (0, 100):
+ #   if i not in(26, 37,84):
+  #      print(i)  
+  ################################################
+#for i in range (1, 100):
+ #   if i % 3 == 0 and i % 5 == 0 : 
+  #      print("FizzBuzz")
+   # elif i % 3 == 0:
+    #    print("Fizz")
+    #elif i % 5 == 0:
+     #   print("Buzz")
+   # else:
+    #    print(i)    
+#######################################################
+
+
+# def My_print_hello(message):
+  #  print(message)
+#My_print_hello("Hello word")
+##########################################
+
+#def My_print_name(name):
+ #   print(name)
+#My_print_name("jean")
+########################################
+
+#def Add(a, b):
+ #   print(a + b)
+#Add(12, 20)
+#######################################
+#def GetHello():
